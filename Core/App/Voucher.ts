54a765f6edfef22d2b518847a20e618c4b2c6f82
huy <lengochuyjob@gmail.com>
2026-02-24 19:12:45 +0700

    CORE - Update Voucher

--- Core/App/Voucher.ts
+++ Core/App/Voucher.ts
@@ -1,27 +1,40 @@
 import * as JD from "decoders"
 import { VoucherID, voucherIDDecoder } from "./Voucher/VoucherID"
-import { Name, nameDecoder } from "./Voucher/VoucherName"
-import { ExpiryDate, expiryDateDecoder } from "./Voucher/ExpiryDate"
-import { DiscountValue, discountValueDecoder } from "./Voucher/DiscountValue"
+import { Active, activeDecoder } from "./Voucher/VoucherActive"
+import { VoucherCode, voucherCodeDecoder } from "./Voucher/VoucherCode"
 import {
-  MinProductValue,
-  minProductValueDecoder,
-} from "./Voucher/MinProductValue"
+  VoucherDiscount,
+  voucherDiscountDecoder,
+} from "./Voucher/VoucherDiscount"
+import { ExpiredDate, expiredDateDecoder } from "./Voucher/VoucherExpiredDate"
+import { UsageLimit, usageLimitDecoder } from "./Voucher/VoucherLimit"
+import {
+  minOrderValueDecoder,
+  MinOrderValue,
+} from "./Voucher/VoucherMinOrderValue"
+import { VoucherName, voucherNameDecoder } from "./Voucher/VoucherName"
+import { UsedCount, usedCountDecoder } from "./Voucher/VoucherUsedCount"
 
 export type Voucher = {
   id: VoucherID
-  name: Name
-  expiryDate: ExpiryDate
-  discountValue: DiscountValue
-  minProductValue: MinProductValue
-  isUsed: boolean
+  active: Active
+  code: VoucherCode
+  discount: VoucherDiscount
+  expiredDate: ExpiredDate
+  limit: UsageLimit
+  minOrderValue: MinOrderValue
+  name: VoucherName
+  usedCount: UsedCount
 }
 
 export const voucherDecoder: JD.Decoder<Voucher> = JD.object({
   id: voucherIDDecoder,
-  name: nameDecoder,
-  expiryDate: expiryDateDecoder,
-  discountValue: discountValueDecoder,
-  minProductValue: minProductValueDecoder,
-  isUsed: JD.boolean,
+  active: activeDecoder,
+  code: voucherCodeDecoder,
+  discount: voucherDiscountDecoder,
+  expiredDate: expiredDateDecoder,
+  limit: usageLimitDecoder,
+  minOrderValue: minOrderValueDecoder,
+  name: voucherNameDecoder,
+  usedCount: usedCountDecoder,
 })
