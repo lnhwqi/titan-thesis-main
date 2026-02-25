90ed62d401c7e2286255bb6505f658e9ee3fd85b
huy <lengochuyjob@gmail.com>
2026-02-25 19:42:09 +0700

    CORE ~ProductBasic(SellerID) ~ProductDetail(SellerID)

--- Core/App/ProductBasic.ts
+++ Core/App/ProductBasic.ts
@@ -1,12 +1,14 @@
 import * as JD from "decoders"
 import { Name, nameDecoder } from "./Product/Name"
 import { ProductID, productIDDecoder } from "./Product/ProductID"
+import { UserID, userIDDecoder } from "./BaseProfile/UserID"
 import { Price, priceDecoder } from "./Product/Price"
 import { ImageUrl, imageUrlDecoder } from "./Product/ProductImageUrl"
 import { CategoryID, categoryIDDecoder } from "./Category/CategoryID"
 
 export type BasicProduct = {
   id: ProductID
+  sellerID: UserID
   name: Name
   price: Price
   url: ImageUrl
@@ -15,6 +17,7 @@ export type BasicProduct = {
 
 export const basicProductDecoder: JD.Decoder<BasicProduct> = JD.object({
   id: productIDDecoder,
+  sellerID: userIDDecoder,
   name: nameDecoder,
   price: priceDecoder,
   url: imageUrlDecoder,
--- Core/App/ProductDetail.ts
+++ Core/App/ProductDetail.ts
@@ -1,6 +1,7 @@
 import * as JD from "decoders"
 import { Name, nameDecoder } from "./Product/Name"
 import { ProductID, productIDDecoder } from "./Product/ProductID"
+import { UserID, userIDDecoder } from "./BaseProfile/UserID"
 import { Price, priceDecoder } from "./Product/Price"
 import { Description, descriptionDecoder } from "./Product/Description"
 import { ImageUrl, imageUrlDecoder } from "./Product/ProductImageUrl"
@@ -8,6 +9,7 @@ import { CategoryID, categoryIDDecoder } from "./Category/CategoryID"
 
 export type DetailProduct = {
   id: ProductID
+  sellerID: UserID
   name: Name
   price: Price
   description: Description
@@ -17,6 +19,7 @@ export type DetailProduct = {
 
 export const productDecoder: JD.Decoder<DetailProduct> = JD.object({
   id: productIDDecoder,
+  sellerID: userIDDecoder,
   name: nameDecoder,
   price: priceDecoder,
   description: descriptionDecoder,
